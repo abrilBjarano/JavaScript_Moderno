06225e2f5e3780fbebf138d418b0f3154e04e198
abrilxcx <abrlbjrn@gmail.com>
2023-01-14 18:02:32 -0600

    crearDeck and pedirCarta functions have been created

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -6,9 +6,71 @@
 */
 
 let deck       = [];
+const numeros  = [2, 3, 4, 5, 6, 7, 8, 9, 10]; 
 let tipos      = ['C','D','H','S'];
 let especiales = ['A','J','Q','K'];
 
 
+/* This function create a new deck */
 
-/* This function create a new deck */ //
+const crearDeck = ( ) => {
+
+    for( let num in numeros){
+        for(let tipo of tipos){
+
+            deck.push( num + tipo);
+        }
+    }
+
+    for( let esp of especiales){
+        for(let tipo of tipos){
+
+            deck.push( esp + tipo);
+        }
+    }
+
+    deck = _.shuffle(deck);
+    console.log(deck);
+} 
+
+crearDeck();
+
+
+/* This function let pick a cart */
+
+const pedirCarta = () => {
+
+    if( deck.length === 0 ){
+        throw 'No hay más cartas en el deck';
+    }
+
+    let carta = deck.shift();
+    console.log( deck );
+    console.log( carta );
+}
+
+pedirCarta();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
